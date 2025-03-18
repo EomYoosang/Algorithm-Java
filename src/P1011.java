7c7dfb61d99ab1ac0169babfcaee8f53ed2a5b84
yoosang <dbtkd1801@gmail.com>
2025-03-18 20:35:54 +0900

    2025-3-18 (2)

--- src/P1011.java
+++ src/P1011.java
@@ -1,7 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class P1011 {
-    public static void main(String[] args) throws IOException {
+    static int t;
+    static int x, y;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            int distance = y - x;
+            int rt = (int) Math.sqrt(distance);
+
+            int result = 0;
+
+            if (distance == rt * rt) {
+                result = 2 * rt - 1;
+            } else if (rt * rt < distance && distance <= rt * rt + rt) {
+                result = 2 * rt;
+            } else {
+                result = 2 * rt + 1;
+            }
+
+            System.out.println(result);
+        }
     }
 }
